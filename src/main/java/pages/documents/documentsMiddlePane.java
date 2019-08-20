fef5b55b4d27fd94362b53cfd0f31a0d520460b3
yehuda <y.d.morad@gmail.com>
2019-08-20 13:25:25 +0300

    update global doc

--- src/main/java/pages/documents/documentsMiddlePane.java
+++ src/main/java/pages/documents/documentsMiddlePane.java
@@ -44,13 +44,76 @@ public class documentsMiddlePane  extends testBase {
 			wait.until(ExpectedConditions.visibilityOf(element));
 			
 	}
+	 
+	 //a function that change the status list to all and archived
+	 public void changeStatusOfList()throws InterruptedException {
 
-	
-	
-		
-		
-	
-	
+		 int sizeOfList = middlepane.listOfEntities.size();
+
+		 actionsmiddlepane.openEntity("test", "importenet2");
+
+		 waitForVisibility(therightonthescreen.pressToChangeStatusOnTheScreen);
+
+		 therightonthescreen.pressToChangeStatusOnTheScreen.click();
+
+
+		 Thread.sleep(2000);
+
+		 therightonthescreen.listOfStatusOnScreen.get(4).click();
+
+
+		 waitForVisibility(middlepane.sortByStatus);
+
+		 middlepane.sortByStatus.click();
+
+
+		 Thread.sleep(2000);
+
+		 middlepane.listOfStatusOfEntities.get(1).click();
+
+		 Thread.sleep(2000);
+
+		 String check = middlepane.checkIfArchvedWorks.getText();
+
+		 // check if the entity exists in archived status
+		 if (check.equals("test")){
+
+			 logger.log(Status.PASS , "status by archived");
+
+		 }
+
+		 else {
+
+			 logger.log(Status.FAIL ,"status by archived");
+		 }
+
+
+		 waitForVisibility(middlepane.sortByStatus);
+
+		 middlepane.sortByStatus.click();
+
+
+		 Thread.sleep(1000);
+
+		 middlepane.listOfStatusOfEntities.get(2).click();
+
+		 Thread.sleep(2000);
+
+		 int sizeOfListAfterPressAll = middlepane.listOfEntities.size();
+
+		 //check if the list is growing when i press on all status
+		 if (sizeOfList < sizeOfListAfterPressAll) {
+
+			 logger.log(Status.PASS , "status by all");
+
+		 }	
+
+		 else {
+
+			 logger.log(Status.FAIL , "status by all");
+		 }
+
+	 }
 			
  }
 	
